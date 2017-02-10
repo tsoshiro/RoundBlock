134827bed77c6e3622c692eedd8513a73159123d
toshiro <tsubasa.oshiro@gmail.com>
2017-02-10 09:46:23 +0900

    上下左右バー調整。

--- RoundBlockProject/Assets/_Scripts/RacketCtrl.cs
+++ RoundBlockProject/Assets/_Scripts/RacketCtrl.cs
@@ -13,19 +13,66 @@ public class RacketCtrl : MonoBehaviour {
 		LEFT,
 		RIGHT
 	}
+	Rigidbody rig;
+
+	#region INPUT
+	public GameObject _touchPositionMarker;
+	public float Z_OFFSET = 7.5f;
+	public bool isVelocity = false;
 
 	Vector3 inputVector;
-	Rigidbody rig;
+	#endregion
+
+	#region MOVE LOGIC
+	public float speed = 20;
+
+	// 移動可能範囲
+	float range_x;
+	float range_z;
+	float rangeRate_z;
+
+	// ステージサイズ : 移動可能範囲算出用
+	public GameObject _wallHorizontal;
+	public GameObject _wallVertical;
+
+	public float STAGE_WIDTH = 10f;
+	public float STAGE_HEIGHT = 22f;
+
+	// ラケットの幅 : localScale
+	float verticalRacketWidth;
+	float horizontalRacketWidth;
+
+	// 基本ラケットと、その他ラケットの位置差分
+	float x_diff_horizontal;
+	float z_diff_horizontal;
+	float z_diff_vertical;
+	#endregion
+
 
 	// Use this for initialization
 	void Start () {
+		Init();
+	}
+
+	void Init() {
 		rig = this.GetComponent<Rigidbody>();
+
+		STAGE_WIDTH = _wallHorizontal.transform.localScale.x;
+		STAGE_HEIGHT = _wallVertical.transform.localScale.x;
+
+		horizontalRacketWidth = this.transform.localScale.x;
+		verticalRacketWidth = otherRackets[(int)OtherRacketPosition.RIGHT].transform.localScale.x;
+
+		// ステージ幅の半分 - ラケットサイズの半分　= 移動可能範囲の絶対値
+		range_x = STAGE_WIDTH * 0.5f - horizontalRacketWidth * 0.5f;
+		range_z = STAGE_HEIGHT * 0.5f - verticalRacketWidth * 0.5f;
+		rangeRate_z = range_z / range_x;
 
 		x_diff_horizontal = otherRackets[(int)OtherRacketPosition.RIGHT].transform.position.x - rig.transform.position.x;
 		z_diff_horizontal = otherRackets[(int)OtherRacketPosition.RIGHT].transform.position.z - rig.transform.position.z;
 		z_diff_vertical = otherRackets[(int)OtherRacketPosition.TOP].transform.position.z - rig.transform.position.z;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		GetInputVector();
@@ -45,12 +92,6 @@ public class RacketCtrl : MonoBehaviour {
 		rig.AddForce(inputVector, ForceMode.Impulse);
 	}
 
-	public GameObject _touchPositionMarker;
-
-	public float speed = 20;
-	public float Z_OFFSET = 7.5f;
-	public bool isVelocity = false;
-
 	void GetTouchPosition() {
 		if (Input.GetMouseButton(0)) {
 			Vector3 screenPos = Input.mousePosition;
@@ -69,18 +110,8 @@ public class RacketCtrl : MonoBehaviour {
 		}
 	}
 
-	float range_x = 5.0f;
-	public float STAGE_WIDTH = 10f;
-	public float STAGE_HEIGHT = 22f;
-	float verticalRacketWidth;
-	float horizontalRacketWidth;
-	float x_diff_horizontal;
-	float z_diff_horizontal;
-	float z_diff_vertical;
-
 	// Transformの方が適切な動きする。
 	void MoveWithTransform(Vector3 worldPos) {
-		range_x = STAGE_WIDTH / this.transform.localScale.x;
 		Vector3 target = new Vector3(worldPos.x, 0, this.transform.position.z);
 		if (target.x <= -range_x) {
 			target.x = -range_x;
@@ -94,7 +125,6 @@ public class RacketCtrl : MonoBehaviour {
 	}
 
 	void MoveWithVelocity(Vector3 worldPos) {
-		range_x = STAGE_WIDTH / this.transform.localScale.x;
 		Vector3 target = new Vector3(worldPos.x, 0, worldPos.z);
 		if (target.x <= -range_x) {
 			target.x = -range_x;
