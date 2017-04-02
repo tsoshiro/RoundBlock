cdc765603f088026f2f876f3a0be30b100c5dad4
toshiro <tsubasa.oshiro@gmail.com>
2017-04-02 22:33:10 +0900

    - キー入力も追加しておく。

--- RoundBlockProject/Assets/_Scripts/InputManager.cs
+++ RoundBlockProject/Assets/_Scripts/InputManager.cs
@@ -19,9 +19,15 @@ public class InputManager : MonoBehaviour {
 	bool isReleased;
 
 	void Update() {
-		setTouchDown();
-		setIsTouching();
-		setIsReleased();
+		if (Input.GetMouseButtonDown (0) ||
+		    Input.GetMouseButton (0) ||
+		    Input.GetMouseButtonUp (0)) {
+			setTouchDown ();
+			setIsTouching ();
+			setIsReleased ();
+		} else {
+			keyControl ();
+		}
 	}
 
 	void setTouchDown() {
@@ -72,4 +78,47 @@ public class InputManager : MonoBehaviour {
 	public bool getIsReleased() {
 		return isReleased;
 	}
+
+	#region Keyboard Control
+	[SerializeField]
+	float KEY_SPEED = 1f;
+
+	void keyControl() {
+		setKeyDown ();
+		setIsKey ();
+		setIsKeyUp ();
+	}
+
+	void setKeyDown() {
+		if (Input.GetKeyDown ("right") ||
+			Input.GetKeyDown ("left") ||
+			Input.GetKeyDown ("space")) {
+			isTapped = true;
+			return;
+		}
+		isTapped = false;
+	}
+
+	void setIsKey() {
+		isTouching = true;
+		if (Input.GetKey ("right")) {
+			worldPosition.x += KEY_SPEED;
+		} else if (Input.GetKey ("left")) {
+			worldPosition.x -= KEY_SPEED;
+		} else {
+			isTouching = false;
+		}
+	}
+
+	void setIsKeyUp() {
+		if (Input.GetKeyDown ("right") ||
+			Input.GetKeyDown ("left") ||
+			Input.GetKeyDown ("space")) {
+			isReleased = true;
+			return;
+		}
+		isReleased = false;
+	}
+
+	#endregion
 }
